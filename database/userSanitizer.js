f7c229ed706b37e83b19f8468c542037772e9d0a
Kekeli <33412200+Kekelii@users.noreply.github.com>
2022-10-27 13:40:36 +0000

    Update userSanitizer.js
    hashing password

--- database/userSanitizer.js
+++ database/userSanitizer.js
@@ -1,12 +1,19 @@
 //this function is responsible for cleaning userdata before been inserted into the database
+import bycript from 'bcrypt'
 
+
+let salt = 10;
 export default async function(values) {
     let userdata = {};
-
     values.forEach((feild) => {
+        if (feild.id == 'password') {
+            userdata[feild.id] = bycript.hashSync(feild.value, salt);
+            return
+        }
         userdata[feild.id] = feild.value
     })
 
+    console.log(userdata)
     return userdata;
 
 }
